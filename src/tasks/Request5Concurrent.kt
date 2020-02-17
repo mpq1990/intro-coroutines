6cb6130b4dde03d4401eca19b2b9c5fe689b7b92
Majid Qureshi <mpq1990@gmail.com>
2020-02-17 23:02:36 +0100

    feat: coroutine concurrency, cancellable, scope, context

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,21 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val deferred: List<Deferred<List<User>>> =  repos.map { repo ->
+        async {
+            log("starting loading for ${repo.name}")
+//            delay(3000)
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+
+    deferred.awaitAll().flatten().aggregate()
 }
